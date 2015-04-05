c08479783f832ff7f8d515ec17442c616d9a4e95
Lewis Collard <lewiscollard@gmail.com>
2015-04-05 18:10:54 +0100

    Huh, apparently none of that title junk is required anymore. Rip out and use add_theme_support('wp-title').

--- functions.php
+++ functions.php
@@ -30,36 +30,9 @@ function blank_widgets_init() {
 
 add_action('widgets_init', 'blank_widgets_init');
 
+add_theme_support('title-tag');
 add_theme_support('automatic-feed-links');
 add_theme_support('post-thumbnails');
-
-function blank_fix_wp_title($title, $sep) {
-    /* Borrowed from twentyfourteen. */
-    global $paged, $page;
-
-    if (is_feed()) {
-        return $title;
-    }
-
-    // Add the site name.
-    $title .= get_bloginfo('name', 'display');
-
-    // Add the site description for the home/front page.
-    $site_description = get_bloginfo( 'description', 'display');
-    if ($site_description && (is_home() || is_front_page())) {
-        $title = "$title $sep $site_description";
-    }
-    
-    // Add a page number if necessary.
-    if ($paged >= 2 || $page >= 2) {
-        $title = "$title $sep " . sprintf(__('Page %s', 'twentyfourteen'), max($paged, $page));
-    }
-    
-    return $title;
-}
-
-add_filter('wp_title', 'blank_fix_wp_title', 10, 2);
-
 
 /* CHANGE THIS */
 if (!isset($content_width)) {
