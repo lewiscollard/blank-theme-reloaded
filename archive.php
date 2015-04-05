cc273bce356764b833a0c7093e131b0049591660
Lewis Collard <lewiscollard@gmail.com>
2015-04-05 18:05:53 +0100

    Rip out a bunch of obsolete things that tried to guess an appropriate archive title. 4.1 does this in a way that makes sense.

--- archive.php
+++ archive.php
@@ -1,22 +1,7 @@
 <?php get_header(); ?>
 
 <?php if (have_posts()) : ?>
-    <?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
-    <?php /* If this is a category archive */ if (is_category()) { ?>
-        <h2>Archive for the &#8216;<?php single_cat_title(); ?>&#8217; Category</h2>
-    <?php /* If this is a tag archive */ } elseif( is_tag() ) { ?>
-        <h2>Posts Tagged &#8216;<?php single_tag_title(); ?>&#8217;</h2>
-    <?php /* If this is a daily archive */ } elseif (is_day()) { ?>
-        <h2>Archive for <?php the_time('F jS, Y'); ?></h2>
-    <?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
-        <h2>Archive for <?php the_time('F, Y'); ?></h2>
-    <?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
-        <h2 class="pagetitle">Archive for <?php the_time('Y'); ?></h2>
-    <?php /* If this is an author archive */ } elseif (is_author()) { ?>
-        <h2 class="pagetitle">Author Archive</h2>
-    <?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-        <h2 class="pagetitle">Blog Archives</h2>
-    <?php } ?>
+    <h2><?php the_archive_title(); ?></h2>
 
     <?php include (TEMPLATEPATH . '/inc/nav.php' ); ?>
 
