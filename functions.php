e2e725aa86c94574fc2debf3ee9a4f20c4da98cb
Lewis Collard <lewiscollard@gmail.com>
2015-12-29 09:48:25 +0000

    Remove emoji shit from header.

--- functions.php
+++ functions.php
@@ -16,6 +16,32 @@ function blank_remove_head_links() {
 add_action('init', 'blank_remove_head_links');
 remove_action('wp_head', 'wp_generator');
 
+// Remove emoji junk.
+// The next two functions are by Christine Cooper:
+// http://wordpress.stackexchange.com/questions/185577/disable-emojicons-introduced-with-wp-4-2
+function blank_disable_emojicons_tinymce($plugins) {
+    if (is_array($plugins)) {
+        return array_diff($plugins, array('wpemoji'));
+    } else {
+        return array();
+    }
+}
+
+function blank_disable_wp_emojicons() {
+    // all actions related to emojis
+    remove_action('admin_print_styles', 'print_emoji_styles');
+    remove_action('wp_head', 'print_emoji_detection_script', 7);
+    remove_action('admin_print_scripts', 'print_emoji_detection_script');
+    remove_action('wp_print_styles', 'print_emoji_styles');
+    remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+    remove_filter('the_content_feed', 'wp_staticize_emoji');
+    remove_filter('comment_text_rss', 'wp_staticize_emoji');
+
+    // filter to remove TinyMCE emojis
+    add_filter('tiny_mce_plugins', 'blank_disable_emojicons_tinymce');
+}
+add_action('init', 'blank_disable_wp_emojicons');
+
 function blank_widgets_init() {
     register_sidebar(array(
         'name' => 'Sidebar Widgets',
